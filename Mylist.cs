b84fd277d8496b74c171798da89acfdc93f8fe56
Logan Demmy <frank.demmy@gmail.com>
2021-06-04 12:02:17 -0400

    phase one complete

---
+++ Mylist.cs
@@ -0,0 +1,17 @@
+namespace mylist
+{
+    class mylist
+    {
+        private string[] _items;
+
+       private int _size = 0;
+
+       public int Count { get; } 
+
+        
+       public mylist(){
+           _items = new string[4];
+           Count = _size;
+       }
+    }
+}
